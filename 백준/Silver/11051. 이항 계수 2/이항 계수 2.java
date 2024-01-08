d76801803fd057c36161d39183e9b9ead344b94c
이시영 <61828877+swy0123@users.noreply.github.com>
2024-01-08 18:17:21 +0900

    [Silver II] Title: 이항 계수 2, Time: 148 ms, Memory: 18332 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+ 
+public class Main {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        
+        int[][] map = new int[1001][1001];
+        
+        map[0][0] = 1;
+        map[1][0] = 1;
+        map[1][1] = 1;
+        for(int i=2; i<=1000; i++) {
+        	map[i][0] = 1;
+        	map[i][i] = 1;
+        	for(int j=1; j<i; j++) {
+        		map[i][j] = (map[i-1][j-1] + map[i-1][j]) % 10007;
+        	}
+        }
+        
+        System.out.println(map[n][k]);
+    }
+}
