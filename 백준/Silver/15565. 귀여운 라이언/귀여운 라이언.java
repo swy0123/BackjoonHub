abba1fce058186e7fb8b6036a558629c0f028221
이시영 <swy05123@gmail.com>
2024-01-17 20:53:34 +0900

    [Silver I] Title: 귀여운 라이언, Time: 444 ms, Memory: 87428 KB -BaekjoonHub

--- 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.java
+++ 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.java
@@ -1,20 +1,16 @@
 import java.util.*;
 import java.io.*;
-
 /*
  * 귀여운 라이언
  */
- 
 public class Main {
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
         
         int[] arr = new int[n];
-        int cnt = 0;
         ArrayList<Integer> list = new ArrayList<>();
         
         st = new StringTokenizer(br.readLine());
@@ -25,12 +21,9 @@ public class Main {
         if(list.size()<k) System.out.println(-1);
         else {
         	int idx = k-1;
-        	int start = 0;
-        	int end = list.get(idx);
         	int min = Integer.MAX_VALUE;
         	while(idx<list.size()) {
         		min = Math.min(min, list.get(idx)-list.get(idx-k+1)+1);
-//        		System.out.println(list+" "+min+" "+list.get(idx)+" "+list.get(idx-k+1));
         		idx++;
         	}
         	System.out.println(min);
