e0feed385caf80562265101f0fc0e4f0f0ef6050
이시영 <swy05123@gmail.com>
2024-02-01 11:50:24 +0900

    [Gold IV] Title: 스타트링크 타워, Time: 156 ms, Memory: 16232 KB -BaekjoonHub

--- 백준/Gold/1089. 스타트링크 타워/스타트링크 타워.java
+++ 백준/Gold/1089. 스타트링크 타워/스타트링크 타워.java
@@ -1,6 +1,8 @@
 import java.util.*;
 import java.io.*;
-
+/*
+ * 스타트링크 타워
+ */
 public class Main {
 	static int[][] pos = {
 			{0,0}, {0, 1}, {0, 2},
@@ -26,36 +28,25 @@ public class Main {
         n = Integer.parseInt(br.readLine());
         width = 4*n-1;
         map = new char[5][width];
-//        System.out.println();
         for(int i=0; i<5; i++) {
         	String str = br.readLine();
         	for(int j=0; j<str.length(); j++) {
         		map[i][j] = str.charAt(j);
         	}
-//        	System.out.println(str);
         }
-//        for(int i=0; i<5; i++) {
-//        	System.out.println(Arrays.toString(map[i]));
-//        }
         check = new boolean[n][10];
         int curPos;
         for(int i=0; i<n; i++) {
         	curPos = i*4;
         	for(int d=0; d<15; d++) {
-//        		System.out.println(d);
         		if(map[pos[d][0]][(curPos+pos[d][1])]=='#') {
         			if(imposible[d][0] == -1) {
         				System.out.println(-1);
-//        				System.out.println(i+" "+d+" "+imposible[d][0]+" "+map[pos[d][0]][curPos+pos[d][1]]+" "+pos[d][0]+" "+(curPos+pos[d][1]));
         				System.exit(0);
         			}
-        			else if(imposible[d][0] == 10) {
-        				continue;
-        			}
+        			else if(imposible[d][0] == 10) continue;
         			else {
-        				for(int k=0; k<imposible[d].length; k++) {
-        					check[i][imposible[d][k]] = true;
-        				}
+        				for(int k=0; k<imposible[d].length; k++) check[i][imposible[d][k]] = true;
         			}
         		}
         	}
@@ -63,7 +54,6 @@ public class Main {
         
         sum = 0;
         for(int i=0; i<n; i++) {
-//        	System.out.println(Arrays.toString(check[i]));
         	double num = 0;
         	int cnt = 0;
         	for(int j=0; j<check[i].length; j++) {
@@ -73,7 +63,6 @@ public class Main {
         	}
         	num *= Math.pow(10, n-i-1);
         	num /= cnt;
-//        	System.out.println(num +" "+ sum+ " "+ cnt);
         	sum += num;
         }
         System.out.printf("%.5f", sum);
