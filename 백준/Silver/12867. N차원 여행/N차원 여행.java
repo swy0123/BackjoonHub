99e38ff2f4e5522fe06eee6e4df0c16b222f1c24
이시영 <swy05123@gmail.com>
2024-02-09 23:58:54 +0900

    [Silver II] Title: N차원 여행, Time: 128 ms, Memory: 14384 KB -BaekjoonHub

---
+++ 백준/Silver/12867. N차원 여행/N차원 여행.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+import java.sql.Array;
+
+public class Main {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        HashSet<Object> set = new HashSet<>();
+
+        int[] val = new int[m];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<m; i++) {
+        	val[i] = Integer.parseInt(st.nextToken());
+        	map.put(val[i], 0);
+        }
+        char[] dir = new char[m];
+        String str = br.readLine();
+        for(int i=0; i<m; i++) {
+        	dir[i] = str.charAt(i);
+        }
+        StringBuilder sb = new StringBuilder();
+        for(Integer num : map.values()) {
+        	sb.append(num);
+        }
+        set.add(sb.toString());
+        
+        for(int i=0; i<m; i++) {
+        	if(dir[i]=='+') map.put(val[i], map.get(val[i])+1);
+        	else map.put(val[i], map.get(val[i])-1);
+
+        	sb = new StringBuilder();
+            for(Integer num : map.values()) {
+            	sb.append(num);
+            }
+            set.add(sb.toString());
+        }
+//    	System.out.println(map);
+        if(set.size()==m+1) System.out.println(1);
+        else System.out.println(0);
+    }
+}
