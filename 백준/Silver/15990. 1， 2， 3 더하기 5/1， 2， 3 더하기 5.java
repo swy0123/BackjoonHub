ccee1eb644f7119d83f68ece0a91eb4615bd72cd
이시영 <swy05123@gmail.com>
2024-02-10 19:23:45 +0900

    [Silver II] Title: 1, 2, 3 더하기 5, Time: 200 ms, Memory: 21184 KB -BaekjoonHub

---
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        int n, size = 5, input[] = new int[t];
+        long dp[][];
+    	
+        for(int i=0; i<t; i++) {
+        	input[i] = Integer.parseInt(br.readLine());
+        	size = Math.max(size, input[i])+1;
+        }
+        
+        dp = new long[size][3];
+        dp[0][0] = 0; dp[0][1] = 0; dp[0][2] = 0;
+        dp[1][0] = 1; dp[1][1] = 0; dp[1][2] = 0;
+        dp[2][0] = 0; dp[2][1] = 1; dp[2][2] = 0;
+        dp[3][0] = 1; dp[3][1] = 1; dp[3][2] = 1;
+        dp[4][0] = 2; dp[4][1] = 0; dp[4][2] = 1;
+        
+        for(int i=5; i<size; i++) {
+        	dp[i][0] = (dp[i-1][1] + dp[i-1][2])%1000000009;
+        	dp[i][1] = (dp[i-2][0] + dp[i-2][2])%1000000009;
+        	dp[i][2] = (dp[i-3][0] + dp[i-3][1])%1000000009;
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<t; i++) {
+        	n = input[i];
+        	sb.append(((dp[n][0]+dp[n][1]+dp[n][2])%1000000009)+"\n");
+        }
+    	System.out.println(sb);
+    }
+}
