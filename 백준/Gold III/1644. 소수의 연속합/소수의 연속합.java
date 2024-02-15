643cb5f6c8f10209a24de7d4a7c31b93f5161c85
이시영 <swy05123@gmail.com>
2024-02-15 13:34:49 +0900

    [Gold III] Title: 소수의 연속합, Time: 240 ms, Memory: 28088 KB -BaekjoonHub

---
+++ 백준/Gold III/1644. 소수의 연속합/소수의 연속합.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        boolean[] v = new boolean[n+1];
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i=2; i<=n; i++) {
+        	if(v[i]) continue;
+        	v[i] = true;
+        	arr.add(i);
+        	for(int j=i+i; j<=n; j+=i) {
+        		v[j] = true;
+        	}
+        }
+        int start=0, end=0, cnt=0, sum, idx;
+        while(start<arr.size() && end<arr.size()) {
+        	idx = start;
+        	sum = 0;
+        	while(idx<=end) {
+        		sum += arr.get(idx);
+        		idx++;
+        	}
+        	if(sum<n) end++;
+        	else if(sum>n)start++;
+        	else {
+        		cnt++;
+        		start++;
+        	}
+        }
+        System.out.println(cnt);
+    }
+}
