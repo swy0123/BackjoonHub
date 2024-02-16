94f29811451aa586c314e0276e12834c44c9154c
이시영 <swy05123@gmail.com>
2024-02-16 12:36:40 +0900

    [Bronze V] Title: A×B, Time: 124 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze V/10998. A×B/A×B.js
+++ 백준/Bronze V/10998. A×B/A×B.js
@@ -1,6 +1,6 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split('/n');
 let line = input[0].split(' ');
-let a = Number(line[0]);
-let b = Number(line[1]);
+let a = parseInt(line[0]);
+let b = parseInt(line[1]);
 console.log(a * b);
