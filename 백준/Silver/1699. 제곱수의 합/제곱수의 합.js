e3803906d8e87d90be4ef4ebe6d6bc921cbe7ae4
이시영 <swy05123@gmail.com>
2024-03-09 22:53:09 +0900

    [Silver II] Title: 제곱수의 합, Time: 244 ms, Memory: 12756 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.js
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.js
@@ -1,16 +1,12 @@
-const fs = require('fs');
+const fs = require("fs");
 const n = parseInt(fs.readFileSync('/dev/stdin').toString().trim());
-
-let dp = Array(n+1).fill(0);
-for(let i=1; i<=n; i++){
-    dp[i] = i;
-}
-for(let i=2; i<=n; i++){
-    let idx = 1;
-    while(idx*idx<=n){
-        if(dp[i]>dp[i-idx*idx]+1) dp[i] = dp[i-idx*idx]+1;
-        idx++;
+let dp = [0];
+for (let i = 1; i <= n; i++) {
+  dp[i] = i;
+  for(let j = 1; j * j <= i; j++) {
+    if(dp[i] > dp[i - j * j] + 1) {
+      dp[i] = dp[i - j * j] + 1;
     }
+  }
 }
-
 console.log(dp[n]);
