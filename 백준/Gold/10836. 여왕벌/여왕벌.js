a3b3a680d8e56be70f0980fc30137350ac44a41b
이시영 <swy05123@gmail.com>
2024-03-13 19:48:16 +0900

    [Gold IV] Title: 여왕벌, Time: 3216 ms, Memory: 144464 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.js
+++ 백준/Gold/10836. 여왕벌/여왕벌.js
@@ -2,41 +2,26 @@ const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split('\n');
 
 const [M, N] = input[0].trim().split(' ').map(Number);
+const size = Array(input[1].trim().split(' ').map(Number).reduce((sum, val)=>sum+val, 0,)).fill(N);
 const map = Array.from(Array(M), ()=>Array(M).fill(1));
-const days = [];
-
 for(let i=1; i<=N; i++){
-    days.push(input[i].trim().split(' ').map(Number));
+    const days = input[i].trim().split(' ').map(Number);
+    for(let i=0; i<days[0]; i++){
+        size[i]--;
+    }
+    for(let i=0; i<days[2]; i++){
+        size[size.length-1-i]++;
+    }
 }
-
-for(let i=0; i<N; i++){
-    for(j=M-1; j>0; j--){
-        if(days[i][0] !== 0){
-            days[i][0]--;
-        }
-        else if(days[i][1] !== 0){
-            days[i][1]--;
-            map[j][0] += 1;
-        }
-        else if(days[i][2] !== 0){
-            days[i][2]--;
-            map[j][0] += 2;
-        }
-    }
-
-    for(j=0; j<M; j++){
-        if(days[i][0] !== 0){
-            days[i][0]--;
-        }
-        else if(days[i][1] !== 0){
-            days[i][1]--;
-            map[0][j] += 1;
-        }
-        else if(days[i][2] !== 0){
-            days[i][2]--;
-            map[0][j] += 2;
-        }
-    }
+// console.log(size);
+let idx = 0;
+for(let i=M-1; i>=0; i--){
+    map[i][0] += size[idx];
+    idx++;
+}
+for(let i=1; i<M; i++){
+    map[0][i] += size[idx];
+    idx++;
 }
 
 for(let i=1; i<M; i++){
@@ -44,8 +29,6 @@ for(let i=1; i<M; i++){
         map[i][j] = Math.max(map[i-1][j], map[i][j-1], map[i-1][j-1]);
     }
 }
-
-
 for(let i=0; i<M; i++){
     console.log(map[i].join(' '));
 }
