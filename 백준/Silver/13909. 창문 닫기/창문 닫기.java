5485c456863f06bbcad3fc845410e947875e963d
이시영 <swy05123@gmail.com>
2024-06-08 20:59:52 +0900

    [Silver V] Title: 창문 닫기, Time: 124 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        int num = (int)Math.sqrt(n);
+        
+        System.out.println(num);
+    }
+}
