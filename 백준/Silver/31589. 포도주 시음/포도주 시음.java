5e9fcedfcf11d6a9d57d98ddae5f67d7639008eb
이시영 <swy05123@gmail.com>
2024-09-06 00:03:29 +0900

    [Silver III] Title: 포도주 시음, Time: 656 ms, Memory: 55524 KB -BaekjoonHub

--- 백준/Silver/31589. 포도주 시음/포도주 시음.java
+++ 백준/Silver/31589. 포도주 시음/포도주 시음.java
@@ -1,38 +1,21 @@
 import java.util.*;
 import java.io.*;
-
 public class Main {
-
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        
-        int[] arr = new int[n];
-        
+        int[] a = new int[n];
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++) {
-        	arr[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(arr);
-
+        for(int i=0; i<n; i++) a[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(a);
         long max = 0;
-
     	int x = m/2;
-    	for(int i=n-1; i>=n-x; i--) {
-    		max+=arr[i];
-    	}
-    	for(int i=0; i<x; i++) {
-    		max-=arr[i];
-    	}
-    	if(m%2==0) {
-    		max += arr[x-1];
-    	}
-    	else {
-    		max += arr[n-x-1];
-    	}
+    	for(int i=n-1; i>=n-x; i--)max+=a[i];
+    	for(int i=0; i<x; i++)max-=a[i];
+    	if(m%2==0) max+=a[x-1];
+    	else max+=a[n-x-1];
         System.out.println(max);
     }
 }
